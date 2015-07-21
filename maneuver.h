cb3e268ac08e7ebc09ee7914eef46becfadcd874
iquater <iquatermai@gmail.com>
2015-07-21 23:26:30 +0400

    little enum

--- maneuver.h
+++ maneuver.h
@@ -2,6 +2,13 @@
 #define MANEUVER_H
 #include "orbit.h"
 
+
+enum maneuver_cases
+{
+	maneuver_complanar_increase_periapsis = 1,
+	maneuver_complanar_increase_apsis = 2,
+	maneuver_complanar_increase_apsis_and_periapsis = 3,
+};
 class CProfile
 {
 
