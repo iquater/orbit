1b4a9bd6ad43ccf66e3f634a32db6d0b257e16c7
iquater <iquatermai@gmail.com>
2015-09-14 00:34:34 +0400

    добавлено пользовательское окно

--- example.cpp
+++ example.cpp
@@ -9,22 +9,22 @@ using namespace utilites;
 
 using namespace maneuver;
 
-int main ()
+int amain ()
 {
 	//CEllepticalOrbit * orbit = new CEllepticalOrbit(EarthMass, 6400, 6600, 7600); 
 
-	CEllepticalOrbit * low_orbit = new CEllepticalOrbit(EarthMass, 6400, 6600, 6600);
+	/**CEllepticalOrbit * low_orbit = new CEllepticalOrbit(EarthMass, 6400, 6600, 6600);
 
 	CEllepticalOrbit * gso_orbit = new CEllepticalOrbit(EarthMass, 6400, 6400+35786, 6400+35786);
 
 	kepler_orbit low = low_orbit->GetKeplerOrbitFormat();
-	kepler_orbit gso = gso_orbit->GetKeplerOrbitFormat();
+	kepler_orbit gso = gso_orbit->GetKeplerOrbitFormat();*/
 
 //	maneuver_scheme scheme(low, gso);
 //	maneuver_scheme scheme_rev(gso, low);
 
-	std::vector<utilites::virtual_transfer_orbit> _transfers;
-	maneuver_scheme::change_apocenter(low, 6500 * 1000, _transfers);
+	/*std::vector<utilites::virtual_transfer_orbit> _transfers;
+	maneuver_scheme::change_apocenter(low, 6500 * 1000, _transfers);*/
 
 	/*double a = gso_orbit->GetOrbitPeriod();
 
@@ -35,8 +35,8 @@ int main ()
 	apo->GetTransferTrajectory(0, 7, 2.5, 0.01);*/
 
 
-	while(getchar()!='\n')
-	{;}
+	/*while(getchar()!='\n')
+	{;}*/
 	return 0;
 }
 
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QtGui>
+#include <qcoreapplication.h>
+
+// Include header files for application components.
+// ...
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication app(argc, argv);
+
+	// Set up and show widgets.
+	// ...
+
+	return app.exec();
+}
